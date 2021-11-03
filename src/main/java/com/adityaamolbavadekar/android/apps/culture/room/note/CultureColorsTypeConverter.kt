38a1fcf72bef9ffe9713b4e8a86ff92fc9b0d0e9
Aditya Bavadekar <64344960+AdityaBavadekar@users.noreply.github.com>
2021-11-03 22:41:17 +0530

    Update CultureColorsTypeConverter.kt

--- src/main/java/com/adityaamolbavadekar/android/apps/culture/room/note/CultureColorsTypeConverter.kt
+++ src/main/java/com/adityaamolbavadekar/android/apps/culture/room/note/CultureColorsTypeConverter.kt
@@ -2,14 +2,14 @@ package com.adityaamolbavadekar.android.apps.culture.room.note
 
 class Converter { 
 
-@TypeConverter 
- fun fromColor(cultureColours : CultureColours) : String { 
-  return cultureColours.name
- } 
+   @TypeConverter 
+    fun fromColor(cultureColours : CultureColours) : String { 
+      return cultureColours.name
+    } 
 
 @TypeConverter 
- fun toColor(colour: String): CultureColors {
-  return CultureColours.valueOf(colour) 
- } 
+    fun toColor(colour: String): CultureColors {
+      return CultureColours.valueOf(colour) 
+    } 
 
 }
